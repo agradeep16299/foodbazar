b683ae0816d3ca0ee7338f94b7f853aefb6a7cbf
agradeep16299 <rakshit.bwn@gmail.com>
2022-12-10 23:48:14 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -30,9 +30,9 @@ app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
 );
 app.use((err, req, res, next) => {
  res.status(500).send({ message: err.message });
